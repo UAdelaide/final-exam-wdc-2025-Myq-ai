88111fadcef7d4341a56e8ef08333793182d4720
Myq-ai <2964972900@qq.com>
2025-06-20 09:24:07 +0000

    [automatic commit] 2025-06-20 09:24:07 UTC 1.147.96.151

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).send('Invalid credentials');
     }
 
-    // 保存用户登录信息到 session
+    // Save the user login information to the session
     req.session.user = {
       user_id: rows[0].user_id,
       username: rows[0].username,
