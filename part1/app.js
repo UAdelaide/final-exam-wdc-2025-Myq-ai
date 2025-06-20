0d5b9f6fef693d1614f1443ef5a0540df24d8b2e
Myq-ai <2964972900@qq.com>
2025-06-20 06:28:46 +0000

    [automatic commit] 2025-06-20 06:28:46 UTC 1.147.96.151

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 
 const app = express();
-const port =8080;
+const port = 8080;
 
 const pool = mysql.createPool({
   host: 'localhost',
